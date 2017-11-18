ed28cf1b99ab81e739abaa833c8bbbdb4ccc06b8
Catherine Triandafillou <ctriandafillou@uchicago.edu>
2017-11-18 11:00:24 -0600

    general rolling mean now handles non-unique timepoints

--- R/gen_rolling_mean.R
+++ R/gen_rolling_mean.R
@@ -11,20 +11,30 @@
 #' @return a dataframe with columns "Time" and "fit" representing the rolling mean at each timepoint.
 
 
-gen_rolling_mean <- function(dat, data.column, time.column = "timepoint", k="auto", k_val=0.05, jitter_factor = 0) {
-  y <- dat[data.column]
+gen_rolling_mean <- function(dat, data.column, time.column = timepoint, k="auto", k_val=0.05, jitter_factor = 0) {
+  time.column <- enquo(time.column)
+  data.column <- enquo(data.column)
+  
+  unique.dat <- group_by(dat, !!time.column, add = TRUE) %>%
+    summarise("obs" = median(!!data.column, na.rm = TRUE))
+  
+  y <- unique.dat["obs"]
+  
   if (k == "auto") {
-    auto_k <- round(nrow(dat)*k_val)
+    auto_k <- round(nrow(unique.dat)*k_val)
     if (auto_k %% 2 == 0) {
       auto_k <- auto_k + 1
     }
     print(auto_k)
     fit <- rollmean(y, auto_k, fill = NA)
-    x <- jitter(dat$timepoint, factor = jitter_factor)
+    x <- unique.dat[as.character(time.column)[[2]]]
+    #x <- jitter(dat$timepoint, factor = jitter_factor)
   } else {
     fit <- rollmean(y, k, fill = NA)
-    x <- jitter(dat$timepoint, factor = jitter_factor)
+    x <- unique.dat[as.character(time.column)[[2]]]
+    #x <- jitter(dat$timepoint, factor = jitter_factor)
   }
-  #x <- ifelse(is.integer(k/2), dat$Time[round(k/2):(length(dat$Time)-round(k/2)-1)], dat$Time[round(k/2):(length(dat$Time)-round(k/2))])
+  
   setNames(data.frame(x, fit), c("fit.timepoint", "fit"))
 }
+
