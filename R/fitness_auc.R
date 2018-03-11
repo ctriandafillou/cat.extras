017d71ea8c227f07631de5865228de086f9e1c87
Catherine Triandafillou <ctriandafillou@uchicago.edu>
2018-03-11 16:31:06 -0500

    added auc calculation for fitness measurements

---
+++ R/fitness_auc.R
@@ -0,0 +1,18 @@
+#' Generate area under the curve for fitness data
+#' 
+#' Data should be of the form x = recovery time and y = log ratio of stressed to unstressed.
+#' 
+#' @param dat A dataframe or dataframe subset to act on
+#' @param normalize Boolean; whether or not to normalize the data. Default is FALSE
+#' @export
+#' @return dataframe of area under the fitness curve
+
+
+fitness_auc <- function(dat, normalize = FALSE) {
+  if(normalize){
+    mutate(dat, norm.growth.ratio = -(growth.ratio - max(growth.ratio, na.rm = T)))
+  }
+  
+  auc <- Bolstad2::sintegral(dat$timepoint, dat$norm.growth.ratio)$int
+  setNames(data.frame(auc), c("auc"))
+}
