c7f9ae73b094c158603b812e9eac003279d6279d
Cat Triandafillou <ctriandafillou@uchicago.edu>
2018-05-09 12:50:36 -0500

    fixed bug in size mixture model fitting function that made 'classification' option not work

--- R/fit_mm_size.R
+++ R/fit_mm_size.R
@@ -8,7 +8,7 @@
 fit_mm_size <- function(df, output = "classification", conf = 0.5) {
   mdl <- normalmixEM(df$FSC.W, 2)
   df <- tibble::rowid_to_column(df, "ID")
-  
+
   if (output == "full"){
     df.w.posterior <- data.frame(FSC.W = mdl$x, mdl$posterior, ID = df$ID) %>%
       mutate(lambda.1 = mdl$lambda[1],
@@ -18,37 +18,39 @@ fit_mm_size <- function(df, output = "classification", conf = 0.5) {
              sigma.1 = mdl$sigma[1],
              sigma.2 = mdl$sigma[2], loglik = mdl$loglik) %>%
       full_join(., df, by = c("FSC.W", "ID"))
-    
+
   } else if (output == "posterior") {
     df.w.posterior <- data.frame(FSC.W = mdl$x, mdl$posterior, ID = df$ID) %>%
       full_join(., df, by = c("FSC.W", "ID"))
-    
+
   } else if (output == "classification") {
     if (mdl$mu[1] > mdl$mu[2]) {
       #print(paste(mdl$mu[1], mdl$mu[2], "one greater than 2", sep = " "))
       df.w.posterior <- data.frame(FSC.W = mdl$x, mdl$posterior, ID = df$ID) %>%
         full_join(., df, by = c("FSC.W", "ID")) %>%
-        mutate(classification = factor(ifelse(comp.1 > conf, "high", 
+        mutate(classification = factor(ifelse(comp.1 > conf, "high",
                                               ifelse(comp.2 > conf, "low", "amb")),
                                        levels = c("amb", "low", "high")))
-      
-      }
-    } else if (output == "model") {
-      #reduced.df <- select(df)
-      df.w.posterior <- data.frame(lambda.1 = mdl$lambda[1],
-                                   lambda.2 = mdl$lambda[2],
-                                   mu.1 = mdl$mu[1],
-                                   mu.2 = mdl$mu[2],
-                                   sigma.1 = mdl$sigma[1],
-                                   sigma.2 = mdl$sigma[2], loglik = mdl$loglik)
-    #bind_cols(reduced.df, df.w.posterior)
-    } else {
-      #print(paste(mdl$mu[1], mdl$mu[2], "two greater than 1", spe = " "))
-      df.w.posterior <- data.frame(FSC.W = mdl$x, mdl$posterior, ID = df$ID) %>%
+
+      } else {
+        #print(paste(mdl$mu[1], mdl$mu[2], "two greater than 1", spe = " "))
+        df.w.posterior <- data.frame(FSC.W = mdl$x, mdl$posterior, ID = df$ID) %>%
         full_join(., df, by = c("FSC.W", "ID")) %>%
         mutate(classification = factor(ifelse(comp.2 > conf, "high",
                                               ifelse(comp.1 > conf, "low", "amb")),
                                       levels = c("amb", "low", "high")))
-    }
+     }
+  } else if (output == "model") {
+    #reduced.df <- select(df)
+    df.w.posterior <- data.frame(lambda.1 = mdl$lambda[1],
+                                 lambda.2 = mdl$lambda[2],
+                                 mu.1 = mdl$mu[1],
+                                 mu.2 = mdl$mu[2],
+                                 sigma.1 = mdl$sigma[1],
+                                 sigma.2 = mdl$sigma[2], loglik = mdl$loglik)
+    #bind_cols(reduced.df, df.w.posterior)
+  } else {
+    stop("output must be 'posterior', 'classification', 'model', or 'full'")
   }
+}
 
