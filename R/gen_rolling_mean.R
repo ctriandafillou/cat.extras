023d95521f058f9f27de29f6c1b3bf75ea7db8a2
Catherine Triandafillou <ctriandafillou@uchicago.edu>
2018-05-19 14:09:39 -0500

    updated rolling mean function

--- R/gen_rolling_mean.R
+++ R/gen_rolling_mean.R
@@ -4,14 +4,14 @@
 #'
 #' @param dat A dataframe or dataframe subset to act on.
 #' @param data.column a string representing the name of the column containing the timeseries observations
-#' @param time.column a string representign the name of the column containing the time; default is "timepoint"
+#' @param time.column a string representing the name of the column containing the time; default is "timepoint"
 #' @param k Indicates method to use for binwidth. If k is an integer, this will be the number of samples to include in the averaging bin. If k is "auto", the binwidth will be determined for each subset individually, size will be determined by k_val
 #' @param k_val Only used if k = "auto"; the percentage of the total number of observations that will be averaged over for each subset
 #' @export
 #' @return a dataframe with columns "Time" and "fit" representing the rolling mean at each timepoint.
 
 
-gen_rolling_mean <- function(dat, data.column, time.column = timepoint, k="auto", k_val=0.05, jitter_factor = 0) {
+gen_rolling_mean <- function(dat, data.column, time.column = timepoint, k="auto", k_val=0.05, jitter_factor = 0, align = "center") {
   time.column <- enquo(time.column)
   data.column <- enquo(data.column)
   
@@ -26,11 +26,11 @@ gen_rolling_mean <- function(dat, data.column, time.column = timepoint, k="auto"
       auto_k <- auto_k + 1
     }
     print(auto_k)
-    fit <- rollmean(y, auto_k, fill = NA)
+    fit <- rollmean(y, auto_k, fill = NA, align = align)
     x <- unique.dat[as.character(time.column)[[2]]]
     #x <- jitter(dat$timepoint, factor = jitter_factor)
   } else {
-    fit <- rollmean(y, k, fill = NA)
+    fit <- rollmean(y, k, fill = NA, align = align)
     x <- unique.dat[as.character(time.column)[[2]]]
     #x <- jitter(dat$timepoint, factor = jitter_factor)
   }
