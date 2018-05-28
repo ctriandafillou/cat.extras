2df297e119fe2b5ee4cb909def6e3438afda3244
Catherine Triandafillou <ctriandafillou@uchicago.edu>
2018-05-28 15:34:47 -0500

    added fitting function with baseline equal 1

---
+++ R/fit_induction_sigmoid_base.R
@@ -0,0 +1,35 @@
+#' Fit induction curve data with a sigmoid
+#' 
+#' This function takes raw data from a calibration curve experiment (yeast expressing pHluorin, flow cytometry data) and does all processing steps to construct a calibration curve that maps fluorescence ratio to pH. Takes *raw* (untransformed) FITC and BV510 channels as inputs!
+#' @param df Dataframe or dataframe subset with columns "timepoint", "med.red", "shock.pH.f", and "treatment" (last two are grouping variables if a dataframe subset)
+#' @param start.list Edit the starting fitting parameters; default is list(a=120, b=0.05, c=120, d=1) (keep this form but change numbers)
+#' @param x.cutoff maximum timepoint to consider in fitting; default is "none" (fits on all timepoints)
+#' @param pr.params logical, whether or not to print the final parameters of the fitted model to screen
+#' @param xmax maxiumum x value to fit to
+#' @export
+#' @return dataframe with columns "treatment", "shock.pH.f", "timepoint", and "med.red" for plotting
+
+
+
+fit_induction_sigmoid_base <- function(df, start.list = c(a=1.8, b=0.03, c=120), x.cutoff = "none", pr.params = F,
+                                       xmax = F) {
+  #dataframe must have columns "timepoint", "med.red", "shock.pH.f" (grouping variable)
+  xe = filter(df, timepoint < x.cutoff)$timepoint
+  ye = filter(df, timepoint < x.cutoff)$med.red
+  
+  fitmodel <- nls(ye~a/(1 + exp(-b * (xe-c))) + 0, start=start.list, control = c(maxiter=200))
+  params = coef(fitmodel)
+  
+  #xt <- seq(min(xe), max(xe), by = 0.5)
+  if(xmax) {
+    xt <- seq(0, xmax, by = 0.5)
+  } else{
+    xt <- seq(0, 180, by = 0.5)
+  }
+  yt <- sigmoid(c(params, 0), xt)
+  
+  if(pr.params == T) {print(params)}
+  
+  setNames(data.frame(xt, yt), c("timepoint", "med.red")) %>% mutate(shock.pH.f = unique(df$shock.pH.f), treatment = unique(df$treatment))
+}
+
